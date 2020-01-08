c08deeb80466dacc11d598b5e0aedd22ec9cd39f
fola-ogunsola <54547114+fola-ogunsola@users.noreply.github.com>
2020-01-08 17:36:40 +0100

    third-commit

---
+++ src/views/question.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <applicantSidebar/>
+        </div>
+
+        <div class="assessment-body">
+            <QuestionPage/>
+        </div>
+    </div>
+</template>
+
+<script>
+import applicantSidebar from '@/components/applicantSidebar.vue'
+import QuestionPage from '@/components/QuestionPage.vue'
+export default {
+    name: 'home',
+    components: {
+       applicantSidebar,
+       QuestionPage
+  }
+}
+</script>
+<style scoped>
+.assessment-body {
+    margin-left: 300px;
+    padding: 80px 70px 50px 50px;
+}
+</style>
